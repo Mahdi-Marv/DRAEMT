3f6ea6fe1f60df41ed430cc36b095243968f3ebb
Mahdi-Marv <mahdihajialilue@gmail.com>
2024-04-09 16:13:11 +0330

    fix bugs

--- test_DRAEM.py
+++ test_DRAEM.py
@@ -119,10 +119,10 @@ def test(obj_names, mvtec_path, checkpoint_path, base_model_name, test_id):
 
             if i_batch in display_indices:
                 t_mask = out_mask_sm[:, 1:, :, :]
-                display_images[cnt_display] = gray_rec[0]
-                display_gt_images[cnt_display] = gray_batch[0]
-                display_out_masks[cnt_display] = t_mask[0]
-                # display_in_masks[cnt_display] = true_mask[0]
+                display_images[cnt_display] = gray_rec[0].cpu().detach()
+                display_gt_images[cnt_display] = gray_batch[0].cpu().detach()
+                display_out_masks[cnt_display] = t_mask[0].cpu().detach()
+                # display_in_masks[cnt_display] = true_mask[0].cpu().detach()
                 cnt_display += 1
 
             out_mask_cv = out_mask_sm[0, 1, :, :].detach().cpu().numpy()
