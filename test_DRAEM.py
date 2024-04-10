1cb0c5548c915f955366f66f2514cc2418b4bed2
Mahdi-Marv <mahdihajialilue@gmail.com>
2024-04-10 18:44:20 +0330

    add print(brain)

--- test_DRAEM.py
+++ test_DRAEM.py
@@ -188,6 +188,8 @@ if __name__ == "__main__":
                 ]
 
     with torch.cuda.device(args.gpu_id):
+        print('test time brain')
+
         print("##### test 1 #####")
         test(obj_list, args.data_path, args.checkpoint_path, args.base_model_name, test_id=1)
 
